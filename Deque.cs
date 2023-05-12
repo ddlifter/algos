815b278932e97ee06f7f704adb55b7a30d1fc392
ddlifter <132294753+ddlifter@users.noreply.github.com>
2023-05-12 19:10:30 +0300

    with comments

--- Deque.cs
+++ Deque.cs
@@ -1,6 +1,4 @@
-﻿using System.Xml.Linq;
-
-namespace LinearStructures.Structures;
+﻿namespace LinearStructures.Structures;
 
 // Линейная структура "Дэк"
 public class Deque
@@ -8,7 +6,7 @@ public class Deque
     public Item? head;
     public Item? tail;
     
-    public int Length
+    public int Length //Свойство длины
     {
         get
         {
@@ -17,67 +15,67 @@ public class Deque
             while (current != null)
             {
                 count++;
-                current = current.next;
+                current = current.next;  //Увеличваем счетчик пока не дойдем до конца то есть до нулевого элемента
             }
             return count;
         }
     }
 
-    public bool isEmpty()
+    public bool isEmpty() //Провера на пустоту
     {
-        return this.head == null && this.tail == null;
+        return this.head == null;
     }
 
-    public void Print()
+    public void Print() //Вывод элементов дэка
     {
         Item? current = this.head;
         while (current != null)
         {
-            Console.Write($"{current.value.ToString()} ");
+            Console.Write($"{current.value.ToString()} ");  //Проходим до конца дэка то есть до нулевого элемена
             current = current.next;
         }
         Console.WriteLine();
     }
 
-    public void PushBack(Item newitem)
+    public void PushBack(Item newitem) //Добавить в конец
     {
         if (head == null)
         {
             head = newitem;
         }
         else
-        {
-            tail.next = newitem;
-            newitem.previous = tail;
+        {                                        //Если дэк пустой то добавляем элемент который будет являться и головой и хвостом
+            tail.next = newitem;                 //Иначе новый элемент теперь следующий для прежнего хвоста а хвост теперь предыдущий для нового элемента
+            newitem.previous = tail;      
         }
         tail = newitem;
     }
 
-    public void PushFront(Item newitem)
+    public void PushFront(Item newitem) //Добавить в начало
     {
         Item? temp = head;
-        newitem.next = temp;
+        newitem.next = temp;                   //Создаем промежуточную переменную temp которой присваиваем значение головы для обмена значениями между головой и следующего для головы элемента
         head = newitem;
-        if (head == null)
-        {
+        if (head == null)                      //Если дэк пустой то добавляем элемент который теперь является и головой и хвостом
+        {                                      
             tail = head;
         }
         else
-        {
+        {                                     //Иначе новый элемент это предыдущий для temp которая является бывшей головой
             temp.previous = newitem;
         }
     }
 
-    public Item PopBack()
+    public Item PopBack() //Удаление с конца
     {
         if (head == null)
-            throw new InvalidOperationException("Deque is empty");
+            throw new InvalidOperationException("Deque is empty");  //Ошибка если дэк пустой
         Item? output = tail;
-        if(this.Length == 1)
+        if(this.Length == 1)        //Если в дэке один элемент то присваиваем output значение этого элемента и указываем что голова и хвост теперь null
         {
             head = tail = null;
         }
-        else
+        else                       //Иначе хвостом теперь будет предыдущий для хвоста элемент
         {
             tail = tail.previous;
             tail.next = null;
@@ -85,42 +83,42 @@ public class Deque
         return output;
     }
 
-    public Item PopFront()
+    public Item PopFront() //Удаление с начала
     {
         if (this.isEmpty())
-            throw new InvalidOperationException("Deque is empty");
+            throw new InvalidOperationException("Deque is empty"); //Ошибка если дэк пустой
         Item? output = head;
-        head = head.next;     
+        head = head.next;                      //Присваиваем для головы новое значение(следующий в дэке элемент)
         output.next = null;
         return output;
     }
 
-    public int Get(int index)
+    public int Get(int index) //Чтение элемента по индексу
     {
         int result = 0;
         if (isEmpty())
-            throw new InvalidOperationException("Deque is empty");
+            throw new InvalidOperationException("Deque is empty"); //Ошибка если дэк пустой
 
         if (index > this.Length)
         {
-            throw new Exception("Out of range");
+            throw new Exception("Out of range");      //Ошибка при выходе за границы дэка
         }
 
-        if (index > (this.Length / 2) - 1)
+        if (index > (this.Length / 2) - 1)            //Если индекс находится правее середины то быстрее будет начать перебор дэка с конца(1)
         {
             for (int i = 0; i < (this.Length - index - 1); i++)
             {
-                PushFront(PopBack());
+                PushFront(PopBack()); //(1)    Чтобы дойти до нужного элемента с конца мы удаляем элемент с конца и добавляем его в начало
             }
-            result = tail.value;
+            result = tail.value; //Дойдя до нужного элемента запоминаем его
             for (int i = 0; i < (this.Length - index - 1); i++)
             {
-                PushBack(PopFront());
+                PushBack(PopFront()); //Теперь возвращаем дэк в исходное состояние удаляя элементы из начала и добавляя их в конец пока не придем к исходному состоянию дэка
             }
             return result;
         }
-        else
-        {
+        else //Если же элемент левее середины то делаем все наоборот. Сначала удаляем элементы из начала и добавляем их в конец пока не дойдем до нужного элемента
+        {    //После чего возвращаем дэк в исходное состояние удаляя элементы с конца и добавляя их в начало
             for (int i = 0; i < index; i++)
             {
                 PushBack(PopFront());
@@ -134,8 +132,8 @@ public class Deque
         }
     }
 
-    public void Set(int index, int value)
-    {
+    public void Set(int index, int value) // Изменение элемента по индексу
+    {                                      //Аналогично Get только с изменением значения
         if (isEmpty())
             throw new InvalidOperationException("Deque is empty");
 
@@ -171,14 +169,14 @@ public class Deque
     }
 
 
-    public int this[int index]
+    public int this[int index] //Перегрузка оператора индексирования в []
     {
         get => Get(index);
         set => Set(index, value);
     }
 
 
-    public void BinaryInsertSort()
+    public void BinaryInsertSort() //Бинарная вставка
     {
         for (int i = 1; i < this.Length; i++)
         {
@@ -186,7 +184,7 @@ public class Deque
             int left = 0;       // Левая граница отсортированной части;
             int right = i - 1;  // Правая граница отсортированной части.
 
-            // Бинарный поиск в отсортированной части стэка.
+            // Бинарный поиск в отсортированной части дэка.
             while (left <= right)
             {
                 int middle = (left + right) / 2;
@@ -209,8 +207,8 @@ public class Deque
 
             this[left] = key; // Вставка ключа на его место в отсортированной части.
 
-            Console.Write($"Этап сортировки {i,4}:      ");
-            this.Print();     // Вывод на экран промежуточного состояния стэка.
+            Console.Write($"{i}: ");
+            this.Print();     // Вывод на экран промежуточного состояния дэка.
         }
     }
 
--- Program.cs
+++ Program.cs
@@ -5,18 +5,24 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        int n = 7;
+        int n = 7; //длина дэка
         Deque deque = new Deque();
         Random rnd = new Random();
 
         for (int i = 0; i < n; i++)
         {
-            deque.PushBack(new Item(value: rnd.Next(0, 100)));
+            deque.PushBack(new Item(value: rnd.Next(0, 100)));  //Заполнение дэка
         }
 
-        deque.Print();
+        deque.Print(); //Вывод первоначального состояния дэка
 
-        deque.BinaryInsertSort();
+        Console.WriteLine();
+
+        deque.BinaryInsertSort(); //Сортировка дэка
+
+        Console.WriteLine();
+
+        deque.Print(); //Вывод конечного состояния
 
     }
 }
